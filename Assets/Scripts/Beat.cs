657ea18e2a856c74c3a10e6bcc021638c98aee67
guimendel <guimendeln@gmail.com>
2022-07-15 17:37:30 -0300

    Delay action to beat trigger

--- Assets/Scripts/Beat.cs
+++ Assets/Scripts/Beat.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Beat : MonoBehaviour
 {
     public float beatsPerSecond = 0.5f;
+
+    public UnityEvent beatTrigger;
     
     // Start is called before the first frame update
     void Start()
@@ -22,6 +25,7 @@ public class Beat : MonoBehaviour
             float beatStart = DateTime.Now.Second;
             
             // Debug.Log("BeatTimer");
+            beatTrigger.Invoke();
 
             // How much time has already passed in this beat
             float elapsedTime = DateTime.Now.Second - beatStart;
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,22 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Events;
 
 
 public class PlayerController : MonoBehaviour
 {
+    // === INTERFACE
+    
     public float jumpForce = 20f;
+    
+    // === STATE
+
+    // The action that will be executed in the next beat trigger
+    UnityAction beatAction;
+
+    // === REFS
     
     Rigidbody2D rigidBody;
 
     public void Start() {
         rigidBody = GetComponent<Rigidbody2D>();
     }
+
+    // Performs the current beat action
+    public void PerformBeatAction() {
+        if (beatAction != null) beatAction();
+        beatAction = null;
+    }
     
     public void Jump(InputAction.CallbackContext callbackContext)
     {
         if (callbackContext.performed && callbackContext.ReadValueAsButton()) {
-            rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            beatAction = () => rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
     }
 
