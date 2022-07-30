8418d5f8008d577476125d1079f36aa4f73f5ef2
guimendel <guimendeln@gmail.com>
2022-07-30 15:48:13 -0300

    Adjust song

--- Assets/Scripts/HighLevel/Beat.cs
+++ Assets/Scripts/HighLevel/Beat.cs
@@ -3,128 +3,138 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using Random=UnityEngine.Random;
+using Random = UnityEngine.Random;
 
 public class Beat : MonoBehaviour
 {
-    // === INTERFACE
+  // === INTERFACE
 
-    // BPM of the song
-    [Min(1f)] public float beatsPerMinute = 120f;
+  // BPM of the song
+  [Min(1f)] public float beatsPerMinute = 120f;
 
-    // How many beats in a downbeat cycle
-    public float beatsPerCycle = 8f;
+  // How many beats in a downbeat cycle
+  public float beatsPerCycle = 8f;
 
-    public UnityEvent DownbeatTrigger;
-    public UnityEvent UpbeatTrigger;
+  // Delay before start counting the beats, measured in beats
+  public float beatCountStartDelay = 0.5f;
 
-    public GameObject kickObject;
-    public GameObject hatObject;
-    public GameObject songObject;
+  public UnityEvent DownbeatTrigger;
+  public UnityEvent UpbeatTrigger;
 
-    // === STATE
+  public GameObject kickObject;
+  public GameObject hatObject;
+  public GameObject songObject;
 
-    // Seconds elapsed since song start
-    float initialDspTime;
+  // === STATE
 
-    // When to trigger next downbeat
-    float nextDownbeat;
+  // Seconds elapsed since song start
+  float initialDspTime;
 
-    // When to trigger next upbeat
-    float nextUpbeat;
+  // When to trigger next downbeat
+  float nextDownbeat;
 
-
-    // === PROPERTIES
-    
-    // Duration of a downbeat
-    float SecondsPerBeat => 60f / beatsPerMinute;
-
-    // Duration of a move cycle, in seconds
-    public float SecondsPerCycle => SecondsPerBeat * beatsPerCycle;
-
-    // Song position, in downbeats
-    float SongPositionBeats => SongPosition / SecondsPerBeat;
-
-    // Position of the song, in seconds
-    float SongPosition => (float) AudioSettings.dspTime - initialDspTime;
+  // When to trigger next upbeat
+  float nextUpbeat;
 
 
-    // === REFS
+  // === PROPERTIES
 
-    AudioSource kickSource;
-    AudioSource hatSource;
-    AudioSource songSource;
-    TheDie theDie;
+  // Duration of a downbeat
+  float SecondsPerBeat => 60f / beatsPerMinute;
 
-    // Start is called before the first frame update
-    void Awake()
+  // Duration of a move cycle, in seconds
+  public float SecondsPerCycle => SecondsPerBeat * beatsPerCycle;
+
+  // Song position, in downbeats
+  float SongPositionBeats => SongPosition / SecondsPerBeat;
+
+  // Position of the song, in seconds
+  float SongPosition => (float)AudioSettings.dspTime - initialDspTime;
+
+
+  // === REFS
+
+  AudioSource kickSource;
+  AudioSource hatSource;
+  AudioSource songSource;
+  TheDie theDie;
+
+  // Start is called before the first frame update
+  void Awake()
+  {
+    theDie = FindObjectOfType<TheDie>();
+    kickSource = kickObject.GetComponent<AudioSource>();
+    hatSource = hatObject.GetComponent<AudioSource>();
+    songSource = songObject.GetComponent<AudioSource>();
+
+    DownbeatTrigger ??= new UnityEvent();
+    UpbeatTrigger ??= new UnityEvent();
+
+    EnsureNotNull.Objects(kickSource, hatSource, theDie);
+  }
+
+  private void Start()
+  {
+    // Get song start time
+    initialDspTime = (float)AudioSettings.dspTime;
+
+    // Start song
+    songSource.Play();
+
+    // Init fields
+    nextDownbeat = beatCountStartDelay;
+    nextUpbeat = beatsPerCycle / 2f + beatCountStartDelay;
+
+    AdjustGravity();
+  }
+
+  // Set the gravity according to the bpm
+  private void AdjustGravity()
+  {
+    Physics2D.gravity = Vector2.down * (beatsPerMinute / 3);
+  }
+
+  private void Update()
+  {
+    // Check if reached next downbeat time
+    if (SongPositionBeats >= nextDownbeat)
     {
-        theDie = FindObjectOfType<TheDie>();
-        kickSource = kickObject.GetComponent<AudioSource>();
-        hatSource = hatObject.GetComponent<AudioSource>();
-        songSource = songObject.GetComponent<AudioSource>();
-        
-        DownbeatTrigger ??= new UnityEvent();
-        UpbeatTrigger ??= new UnityEvent();
+      // Advance downbeat
+      while (SongPositionBeats >= nextDownbeat)
+      {
+        nextDownbeat += beatsPerCycle;
+      }
 
-        EnsureNotNull.Objects(kickSource, hatSource, theDie);
+      Downbeat();
     }
 
-    private void Start() {
-        // Get song start time
-        initialDspTime = (float) AudioSettings.dspTime;
+    // Check if reached next upbeat time
+    if (SongPositionBeats >= nextUpbeat)
+    {
+      // Advance upbeat
+      while (SongPositionBeats >= nextUpbeat)
+      {
+        nextUpbeat += beatsPerCycle;
+      }
 
-        // Start song
-        songSource.Play();
-
-        // Register first downbeat
-        Downbeat();
-
-        // Init fields
-        nextDownbeat = beatsPerCycle;
-        nextUpbeat = beatsPerCycle / 2f;
+      Upbeat();
     }
+  }
 
-    private void Update() {
-        // When either a downbeat or upbeat triggers, this is incremented
-        int triggered;
+  void Downbeat()
+  {
+    // First, let the die know
+    theDie.CheckRollDie();
 
-        // Whenever both down and upbeat trigger, recheck if another trigger is necessary
-        do {
-            triggered = 0;
-            
-            // Check if reached next downbeat time
-            if (SongPositionBeats >= nextDownbeat) {
-                // Advance downbeat
-                nextDownbeat += beatsPerCycle;
+    // kickSource.Play();
+    // print("DownbeatTrigger");
+    DownbeatTrigger.Invoke();
+  }
 
-                Downbeat();
-
-                triggered++;
-            }
-
-            // Check if reached next upbeat time
-            if (SongPositionBeats >= nextUpbeat) {
-                // Advance upbeat
-                nextUpbeat += beatsPerCycle;
-
-                Upbeat();
-
-                triggered++;
-            }
-        } while (triggered == 2);
-    }
-
-    void Downbeat() {
-        // First, let the die know
-        theDie.CheckRollDie();
-
-        kickSource.Play();
-        DownbeatTrigger.Invoke();
-    }
-
-    void Upbeat() {
-        hatSource.Play();
-        UpbeatTrigger.Invoke();
-    }
+  void Upbeat()
+  {
+    // hatSource.Play();
+    // print("UpbeatTrigger");
+    UpbeatTrigger.Invoke();
+  }
 }
